b96c7b641c06b0fce38f78b3a2027150a2ebacaf
Dirk H. Wolthuis <hallo@ikbendirk.nl>
2019-06-16 13:08:12 +0200

    implemented database models

---
+++ app/database.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize')
+
+var db = {}
+
+const sequelize = new Sequelize('graphql-mysql-tutorial', 'graphql', '123456', {
+    host: 'localhost',
+    port: '8006',
+    dialect: 'mysql',
+    define: {
+        freezeTableName: true,
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+    operatorsAliases: false,
+})
+
+let models = [
+    require('./models/priorities.js'),
+    require('./models/status.js'),
+    require('./models/tickets.js'),
+    require('./models/users.js'),
+]
+
+// Initialize models
+models.forEach(model => {
+    const seqModel = model(sequelize, Sequelize)
+    db[seqModel.name] = seqModel
+})
+
+// Apply associations
+Object.keys(db).forEach(key => {
+    if ('associate' in db[key]) {
+        db[key].associate(db)
+    }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
