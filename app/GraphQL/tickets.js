ca4769786caf3071724ae52ffe89eacc3419d3fa
ikbendirk <hallo@ikbendirk.nl>
2019-07-19 20:46:30 +0200

    fin tutorial

--- app/GraphQL/tickets.js
+++ app/GraphQL/tickets.js
@@ -11,10 +11,13 @@ export const typeDefs = gql`
         id: ID!
         subject: String
         priority_id: Int
+        priority: Priority
         status_id: Int
+        status: Status
         user_id: Int
         user: User
         assigned_to_user_id: Int
+        assigned_to_user: User
     }
 `
 
@@ -27,5 +30,11 @@ export const resolvers = {
     Ticket: {
         user: async (obj, args, context, info) =>
             db.users.findByPk(obj.user_id),
+        priority: async (obj, args, context, info) =>
+            db.priorities.findByPk(obj.priority_id),
+        status: async (obj, args, context, info) =>
+            db.status.findByPk(obj.status_id),
+        assigned_to_user: async (obj, args, context, info) =>
+            db.users.findByPk(obj.assigned_to_user_id),
     },
 }
